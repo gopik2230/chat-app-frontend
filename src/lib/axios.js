3c89f90d85c922660f083ed8a00e9492d43edd6f
gopik2230 <gopik2230@gmail.com>
2025-04-22 20:45:12 +0530

    urlUpdated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001" : '',
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001" : 'https://chat-app-backend-a0h3.onrender.com',
     withCredentials: true,
 })
